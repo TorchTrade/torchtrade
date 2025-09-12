fix feature computation and lookback issue

trading_envs/alpaca/obs_class.py
@@ -69,7 +69,7 @@ class AlpacaObservationClass:
         return np.array(df[columns], dtype=np.float32)
 
     def _fetch_single_timeframe(
-        self, timeframe: TimeFrame, window_size: int
+        self, timeframe: TimeFrame #, window_size: int
     ) -> pd.DataFrame:
         """Fetch and preprocess data for a single timeframe."""
         if timeframe.unit == TimeFrameUnit.Day and self.default_lookback > self.default_lookback:
@@ -81,7 +81,7 @@ class AlpacaObservationClass:
             timeframe=timeframe,
             start=now - timedelta(days=self.default_lookback), # TODO: this is critical
             end=now,
-            limit=window_size
+            #limit=window_size
         )
         return self.client.get_crypto_bars(request).df
 
@@ -134,7 +134,7 @@ class AlpacaObservationClass:
 
         for timeframe, window_size in zip(self.timeframes, self.window_sizes):
             key = f"{timeframe.amount}{timeframe.unit.name}_{window_size}"
-            df = self._fetch_single_timeframe(timeframe, window_size)
+            df = self._fetch_single_timeframe(timeframe)
             
             # Store base OHLC features if this is the first timeframe and return_base_ohlc is True
             if return_base_ohlc and timeframe == self.timeframes[0]:
@@ -147,6 +147,8 @@ class AlpacaObservationClass:
                 )
                 observations['base_timestamps'] = base_df['timestamp'].values
             processed_df = self.feature_preprocessing_fn(df)
+            # apply window size
+            processed_df = processed_df.iloc[-window_size:]
             observations[key] = self._get_numpy_obs(processed_df)
 
         return observations

trading_envs/alpaca/torch_env.py
@@ -102,14 +102,15 @@ class AlpacaTorchTradingEnv(EnvBase):
     def _get_observation(self) -> TensorDictBase:
         """Get the current observation state."""
         # Get market data
-        obs_dict = self.observer.get_observations(return_base_ohlc=True)
+        obs_dict = self.observer.get_observations(return_base_ohlc=True if self.config.include_base_features else False)
         market_data = obs_dict[self.observer.get_keys()[0]]
 
-        base_features = obs_dict["base_features"]
-        base_timestamps = obs_dict["base_timestamps"]
-        # Convert to Unix timestamps (seconds)
-        timestamps = base_timestamps.astype('datetime64[s]').astype(np.int64)
-        base_timestamps = torch.from_numpy(timestamps)
+        if self.config.include_base_features:
+            base_features = obs_dict["base_features"]
+            base_timestamps = obs_dict["base_timestamps"]
+            # Convert to Unix timestamps (seconds)
+            timestamps = base_timestamps.astype('datetime64[s]').astype(np.int64)
+            base_timestamps = torch.from_numpy(timestamps)
         
         market_data = [obs_dict[features_name] for features_name in self.observer.get_keys()]
 