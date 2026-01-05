102606963810220edf7dc91138eebab832a43e96
BY571 <sebastian.dittert@gmx.de>
2026-01-05 12:08:46 +0100

    Update ppo futures onestep example test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -442,6 +442,15 @@ EXAMPLE_COMMANDS = {
         "logger.backend= "
         "logger.test_interval=1000000 "
     ),
+    "ppo_futuresonestep": (
+        "python examples/online/ppo_futures_onestep/train.py "
+        "collector.total_frames=100 "
+        "collector.frames_per_batch=50 "
+        "env.train_envs=2 "
+        "loss.mini_batch_size=25 "
+        "logger.backend= "
+        "logger.test_interval=1000000 "
+    ),
     "ppo_seqfutures": (
         "python examples/online/ppo_futures/train.py "
         "collector.total_frames=100 "
